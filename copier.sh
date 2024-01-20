df5eb1e3de642d205cd9f481b00e96a3cbcb76d2
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2024-01-20 19:21:05 -0300

    test update

--- copier.sh
+++ copier.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-TEST_NAME=large-image
+TEST_NAME=event-bridge
 cp ~/Projects/personal/haystack/images/"$TEST_NAME".png src/test/resources/"$TEST_NAME"/
+cp ~/Projects/personal/haystack/images/"$TEST_NAME"-modified.png src/test/resources/"$TEST_NAME"/
 cp ~/Projects/personal/haystack/images/decompressed_data_bytes src/test/resources/"$TEST_NAME"/
 cp ~/Projects/personal/haystack/images/compressed_data_bytes src/test/resources/"$TEST_NAME"/
 cp ~/Projects/personal/haystack/images/filtered_data_bytes src/test/resources/"$TEST_NAME"/
